012d7fe189432bd25caa6b898ebd1f911b609898
dnaputi92 <85891724+dnaputi92@users.noreply.github.com>
2022-02-13 16:47:31 -0800

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = https://f8aec05a.us-south.apigw.appdomain.cloud/guestbook;
+const apiUrl = https://5b6af838.us-south.apigw.appdomain.cloud/guestbook;
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
